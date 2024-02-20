2bbef434888f028b0906af870ab1c893f5f400c2
viktormashyka <99292051+viktormashyka@users.noreply.github.com>
2024-02-20 22:23:34 +0000

    added js

--- src/artists-page.js
+++ src/artists-page.js
@@ -3,5 +3,6 @@ import 'izitoast/dist/css/iziToast.min.css';
 // import './js/dark-theme-switch.js';
 // import './js/modal-main.js';
 import './js/header.js';
-// import './js/burger-menu.js';
+import './js/scroll-up.js';
+import './js/burger-menu.js';
 // import './js/favorites.js';
