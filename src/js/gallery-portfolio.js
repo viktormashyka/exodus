fc69252492f2fe918dd3073a9c4a68817d5793ed
viktormashyka <99292051+viktormashyka@users.noreply.github.com>
2024-05-12 21:31:09 +0100

    test image 8

--- src/js/gallery-portfolio.js
+++ src/js/gallery-portfolio.js
@@ -8,13 +8,11 @@ const placeholder = '../images/tattoo-placeholder.png';
 
 const markup = gallery
   .map(el => {
-    return `<li class="gallery__item">
-  <a class="gallery__link" href=${el.original}>
-    <img class="gallery__image" src=${el.original ?? placeholder} data-src="${
-      el.original ?? placeholder
-    }" alt=${description} title=${description}  loading="lazy"/>
-  </a>
-</li>`;
+    return `        <img
+          class="about-img"
+          src="./images/about-desktop.webp"
+          alt="Artist makes tattoo"
+        />`;
   })
   .join('');
 
@@ -26,3 +24,11 @@ var lightbox = new SimpleLightbox('.gallery a', {
   captionPosition: 'bottom',
   captionDelay: 250,
 });
+
+// `<li class="gallery__item">
+//   <a class="gallery__link" href=${el.original}>
+//     <img class="gallery__image" src=${el.original ?? placeholder} data-src="${
+//   el.original ?? placeholder
+// }" alt=${description} title=${description}  loading="lazy"/>
+//   </a>
+// </li>`;
