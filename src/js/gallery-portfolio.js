d4b35da9e76068142a8c24ddc3e699eda7395b57
viktormashyka <99292051+viktormashyka@users.noreply.github.com>
2024-06-05 09:12:56 +0100

    revert last success commit

--- src/js/gallery-portfolio.js
+++ src/js/gallery-portfolio.js
@@ -4,13 +4,17 @@ const galleryList = document.querySelector('.gallery');
 
 const description = 'Tattoo made by Exodus';
 const placeholder = './images/tattoo-placeholder.png';
-const pathTo = '../../src/';
-const img = './images/gallery/tattoo-1.webp';
 
 const markup = galleryItems.data
   .map(
     el =>
-      `<li class="gallery__item"><a class="gallery__link" href=${img}><picture><img class="gallery__image" src=${img} data-src="${img}" alt=${description} title=${description}  loading="lazy"/></picture></a></li>`
+      `<li class="gallery__item"><a class="gallery__link" href=${
+        el.original
+      }><picture><img class="gallery__image" src=${
+        el.original ?? placeholder
+      } data-src="${
+        el.original ?? placeholder
+      }" alt=${description} title=${description}  loading="lazy"/></picture></a></li>`
   )
   .join('');
 
@@ -23,11 +27,3 @@ var lightbox = new SimpleLightbox('.gallery a', {
   captionPosition: 'bottom',
   captionDelay: 250,
 });
-
-// `<li class="gallery__item"><a class="gallery__link" href=${
-//   pathTo + el.original
-// }><picture><img class="gallery__image" src=${
-//   pathTo + el.original ?? placeholder
-// } data-src="${
-//   pathTo + el.original ?? placeholder
-// }" alt=${description} title=${description}  loading="lazy"/></picture></a></li>`;
